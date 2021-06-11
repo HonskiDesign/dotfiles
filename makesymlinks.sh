93c1df172bb5b10b0c596d0d2365be48bd7d7718
HonskiDesign <garren@honski.design>
2021-06-10 20:50:05 -0700

    Initialize Repo

---
+++ makesymlinks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script creates symlinks from the home directory to any desired dotfile in ~dotfiles
+
+
+# Variables
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="bashrc vimrc"
+
+
+# backup dotfiles to dotfiles_old
+echo "Creating $olddir for backup of an existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move an existing dotfiles in ~/ to dotfiles_old, then create symlinks
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
---
+++ vimrc
@@ -0,0 +1,14 @@
+set number
+set showcmd
+set tabstop=4
+set shiftwidth=4
+set expandtab
+syntax enable
+
+"Plugins
+
+call plug#begin('~/.vim/plugged')
+
+"Plug 'gabrielelana/vim-markdown'
+
+call plug#end()
